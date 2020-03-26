78dbb038e709e75b1a2470607e14c8a55e398dae
Turingas <62706285+Turingas@users.noreply.github.com>
2020-03-26 18:18:27 +0200

    Update __init__.py

--- 2019/__init__.py
+++ 2019/__init__.py
@@ -7,27 +7,6 @@ def exists():
     """programa.cpp egzistuoja"""
     check50.exists("programa.cpp")
     check50.include("1.txt", "2.txt")
-
-#@check50.check(exists)
-#def compiles():
-#    """U1.c kompiliuojasi"""
-#    check50.c.compile("U1.c", lcs50=True)
-
-#bandymas patikrinti ar .cpp file'as kompiliuojasi
-# bet neveikia: error: invalid argument '-std=c11' not allowed with 'C++'
-#@check50.check(exists)
-#def compiles1():
-#    """testU1.cpp kompiliuojasi"""
-#    check50.c.compile("testU1.cpp", lcs50=True)
-
-# bandymas paleisti sukompiliuota .cpp file'ą, bet nesigauna.
-#@check50.check(compiles)
-#def testingCPP():
-#    """Ar pasileidžia sukompiliuotas CPP file'as"""
-#    out = check50.run("./testU1").stdin("1").stdout()
-#    out = check50.run("./testU1").stdout()
-#    compare_values(out, open("2.txt").read())
-#    check50.run("./testU1").stdout(1)
     
 @check50.check(exists)
 def isOutput():
@@ -51,7 +30,26 @@ def compare_files(output, correct):
         return 
     raise check50.Mismatch(correct, output, help= None)
 
+#@check50.check(exists)
+#def compiles():
+#    """U1.c kompiliuojasi"""
+#    check50.c.compile("U1.c", lcs50=True)
 
+#bandymas patikrinti ar .cpp file'as kompiliuojasi
+# bet neveikia: error: invalid argument '-std=c11' not allowed with 'C++'
+#@check50.check(exists)
+#def compiles1():
+#    """testU1.cpp kompiliuojasi"""
+#    check50.c.compile("testU1.cpp", lcs50=True)
+
+# bandymas paleisti sukompiliuota .cpp file'ą, bet nesigauna.
+#@check50.check(compiles)
+#def testingCPP():
+#    """Ar pasileidžia sukompiliuotas CPP file'as"""
+#    out = check50.run("./testU1").stdin("1").stdout()
+#    out = check50.run("./testU1").stdout()
+#    compare_values(out, open("2.txt").read())
+#    check50.run("./testU1").stdout(1)
     
 # Skirta .cpp output patikrinimui
 #def compare_values(output, correct):
